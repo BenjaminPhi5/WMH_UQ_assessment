a0d1d6594c416ef388ffb6f5c35a2d60e758a334
Ben Philps <s2208943@ed.ac.uk>
2023-11-29 20:49:18 +0000

    test scratch folder detection

--- slurm_cluster/journal_experiments/test_cluster.sh
+++ slurm_cluster/journal_experiments/test_cluster.sh
@@ -12,3 +12,15 @@ set -e
 CONDA_ENV_NAME=wmh
 echo "Activating conda environment: ${CONDA_ENV_NAME}"
 source activate ${CONDA_ENV_NAME}
+
+if [ -d "/disk/scratch_big" ]; then
+  SCRATCH_DISK=/disk/scratch_big
+else
+  SCRATCH_DISK=/disk/scratch
+fi
+
+SCRATCH_HOME=${SCRATCH_DISK}/${USER}
+
+mkdir -p ${SCRATCH_HOME}
+
+echo ${SCRATCH_HOME}
