6136eba56de3fe19a871b1e76cc39cdf6d0f27e1
Ben Philps <s2208943@ed.ac.uk>
2024-02-16 16:50:32 +0000

    bugfix the wrong dataset parser being used for the kjf preprocessor

--- twaidata/MRI_preprep/remote_run_LBCkjf.sh
+++ twaidata/MRI_preprep/remote_run_LBCkjf.sh
@@ -5,4 +5,4 @@ in_dir=/home/s2208943/ipdis/data/InterRater_data
 out_dir=/home/s2208943/ipdis/data/preprocessed_data/LBCkjf_InterRaterData
 
 # process MSS3 dataset
-python simple_preprocess_st2.py -i ${in_dir} -o ${out_dir} -n LBC -s 0 -e -1 -f False -z True -a False -k False
+python simple_preprocess_st2.py -i ${in_dir} -o ${out_dir} -n LBCkjf -s 0 -e -1 -f False -z True -a False -k False
